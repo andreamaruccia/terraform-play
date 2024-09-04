7307c6df8ac1368aa5bf7d93ff4045b9e35a859b
Andrea Maruccia <eltuko@gmail.com>
2024-09-04 17:54:32 +0200

    creating some errors for fmt and lint

--- tf/main.tf
+++ tf/main.tf
@@ -15,10 +15,10 @@ terraform {
   # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
   # forwards compatible with 0.13.x code.
-  required_version = ">= 1.9.5"
+  #   required_version = ">= 1.9.5"
 }
 
 # website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
 output "hello_world" {
-  value = "Hello, World!"
+  value =                      "Hello, World!"
 }
